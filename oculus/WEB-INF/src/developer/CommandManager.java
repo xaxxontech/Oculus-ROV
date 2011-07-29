792597258bdcbddbc39528dd9bee1f5fd9497f83
brad.zdanivsky <brad.zdanivsky@dc971243-0a89-dcf2-b47a-c166fffa500b>
2011-07-29 20:26:34 +0000

    packages moved

--- oculus/WEB-INF/src/developer/CommandManager.java
+++ oculus/WEB-INF/src/developer/CommandManager.java
@@ -10,7 +10,6 @@ import oculus.Util;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 
-
 /** */
 public class CommandManager {
 
@@ -18,13 +17,14 @@ public class CommandManager {
 	private static String oculus = "oculus";
 	private static String function = "function";
 	private static String argument = "argument";
-	private static Logger log = Red5LoggerFactory.getLogger(CommandManager.class, oculus);
+	private static Logger log = Red5LoggerFactory.getLogger(
+			CommandManager.class, oculus);
 	private State state = State.getReference();
 	private static Application app = null;
 	private MulticastChannel channel = null;
 
 	/** */
-	public CommandManager(Application a){ 
+	public CommandManager(Application a) {
 		app = a;
 		channel = new MulticastChannel(this);
 		log.debug("command manager ready");
@@ -38,62 +38,60 @@ public class CommandManager {
 
 	/** try to find dock by turning in a circle and scanning */
 	public void home() {
-		
-		//new Thread() {
-			//public void run() {
 
-				System.out.println("...starting to find home....");
-				
-				while( !state.equals(State.status, State.docked)) {
+		System.out.println("...starting to find home....");
 
-					if (! state.getBoolean("motionenabled")){
-						System.out.println("motion enabled.. ");
-						return;
-					}
-					
-					if (state.get("status").equalsIgnoreCase("docked")){
-						System.out.println("...home found....");
-						return;
-					}
-					
-					app.playerCallServer(Application.playerCommands.nudge, "left");
-					Util.delay(3000);
-					app.playerCallServer(Application.playerCommands.move, "stop");
-					app.playerCallServer(Application.playerCommands.autodock, "go");
-				
-				}
-				
-		//	}
-//}.start();
-	
+		while (!state.equals(State.dockstatus, State.docked)) {
+
+			if (!state.getBoolean("motionenabled")) {
+				System.out.println("motion enabled.. ");
+				return;
+			}
+
+			if (state.get("status").equalsIgnoreCase("docked")) {
+				System.out.println("...home found....");
+				return;
+			}
+
+			app.playerCallServer(Application.playerCommands.nudge, "left");
+			Util.delay(3000);
+			app.playerCallServer(Application.playerCommands.move, "stop");
+			app.playerCallServer(Application.playerCommands.autodock, "go");
+
+		}
+
 	}
 
 	/** send email to admin account */
-	public void dump(){
+	public void dump() {
 		new Thread() {
 			public void run() {
 
-				final String temp = System.getenv("RED5_HOME")+"\\log\\debug.txt";
-				final String move = System.getenv("RED5_HOME")+"\\log\\moves.log";
+				final String temp = System.getenv("RED5_HOME")
+						+ "\\log\\debug.txt";
+				final String move = System.getenv("RED5_HOME")
+						+ "\\log\\moves.log";
 
-				// delete if exists from before 
+				// delete if exists from before
 				new File(temp).delete();
-				
-				// write current state to file
-				state.writeFile(temp);			
-										
-				// blocking send 
-				// new SendMail("Oculus State Dump", "debug dump attached", temp, true);
-				// new SendMail("Oculus Move Log", "debug dump attached", move, true);
 
-				// email'ed it, now delete it 
-				new File(temp).delete();	
-				// new File(move).delete();	
+				// write current state to file
+				state.writeFile(temp);
+
+				// blocking send
+				// new SendMail("Oculus State Dump", "debug dump attached",
+				// temp, true);
+				// new SendMail("Oculus Move Log", "debug dump attached", move,
+				// true);
+
+				// email'ed it, now delete it
+				new File(temp).delete();
+				// new File(move).delete();
 
 			}
 		}.start();
 	}
-	
+
 	/** take any message on this channel, look for an action */
 	public void execute(Command command) {
 		if (app == null) {
@@ -102,15 +100,17 @@ public class CommandManager {
 			return;
 		}
 
-		System.out.println("type: " + command.getType() + " cmd: " + command.list());
+		System.out.println("type: " + command.getType() + " cmd: "
+				+ command.list());
 
 		// only listen to <oculus> xml </oculus> messages
 		if (command.getType().equalsIgnoreCase(oculus)) {
-			
+
 			final String fn = command.get(function);
 			final String arg = command.get(argument);
 
-			if (fn != null) {
+			if (fn == null) return; 
+			
 				if (fn.equalsIgnoreCase("home")) {
 
 					new Thread() {
@@ -118,27 +118,29 @@ public class CommandManager {
 							home();
 						}
 					}.start();
-					
+
 				} else if (fn.equalsIgnoreCase("dump")) {
 
 					dump();
-					
+
 				} else if (fn.equalsIgnoreCase("sonar")) {
-					if( arg != null ){
-						if(arg.equals("debug")) state.set(State.sonarDebug, true);
-						if(arg.equals("reset")) state.set(State.sonarDebug, false);
+					if (arg != null) {
+						if (arg.equals("debug"))
+							state.set(State.sonardebug, true);
+						if (arg.equals("reset"))
+							state.set(State.sonardebug, false);
 					}
 				} else if (fn.equalsIgnoreCase("find")) {
 
-					// System.out.println("forward.. to do, merge with exp");
+					
 					
 					try {
+						app.playerCallServer(Application.playerCommands.autodock, "cancel");
 						app.playerCallServer(Application.playerCommands.dockgrab, null);
 					} catch (Exception e) {
 						System.out.println("_app call error");
 					}
-			
-					
+
 				} else { // must be an application primitive
 					try {
 						app.playerCallServer(fn, arg);
@@ -146,7 +148,7 @@ public class CommandManager {
 						System.out.println("_app call error");
 					}
 				}
-			}
+			
 		}
 	}
 }

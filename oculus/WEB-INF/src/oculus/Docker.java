7efdc60ef0e68d5b7ec9435a4f758d34ee939c89
brad.zdanivsky <brad.zdanivsky@dc971243-0a89-dcf2-b47a-c166fffa500b>
2011-07-29 20:23:18 +0000

    interface used for sonar docking option

--- oculus/WEB-INF/src/oculus/Docker.java
+++ oculus/WEB-INF/src/oculus/Docker.java
@@ -4,12 +4,6 @@ public interface Docker {
 
 	public abstract void cancel();
 
-	/** 
-	 * 
-	 * 
-	 * 
-	 * @param str
-	 */
 	public abstract void autoDock(String str);
 
 	public abstract void dock(String str);

3c61214f8f743b4c6c50a8a9f03015279f871922
brad.zdanivsky <brad.zdanivsky@dc971243-0a89-dcf2-b47a-c166fffa500b>
2011-09-17 01:25:40 +0000

    use nested enum to define sub-set of admin only commands

--- oculus/WEB-INF/src/oculus/PlayerCommands.java
+++ oculus/WEB-INF/src/oculus/PlayerCommands.java
@@ -11,6 +11,55 @@ public enum PlayerCommands {
 	softwareupdate, restart, arduinoecho, arduinoreset, setsystemvolume, beapassenger, muterovmiconmovetoggle,
 	spotlightsetbrightness, floodlight, dockgrab, writesetting, holdservo;
 
+	
+	public enum playerOnlyCommands {
+
+		// TODO: CHECK THESE 
+		publish, move, battStats, docklineposupdate, autodock, autodockcalibrate,
+		speech, getdrivingsettings, drivingsettingsupdate, gettiltsettings, cameracommand, tiltsettingsupdate,
+		tilttest, speedset, slide, nudge, dock, clicksteer, chat, statuscheck, systemcall, 
+		streamsettingsset, streamsettingscustom, motionenabletoggle, playerexit, playerbroadcast, password_update, 
+		extrauser_password_update, username_update,
+		disconnectotherconnections, monitor, framegrab, emailgrab, assumecontrol, 
+		softwareupdate, setsystemvolume, beapassenger, 
+			
+	}
+	
+
+	/**
+	 * @return true if given command is in the sub-set
+	 */
+	public static boolean requiresPlayer(PlayerCommands cmd){
+		for(playerOnlyCommands admin : playerOnlyCommands.values()){
+			if(admin.equals(cmd)) 
+				return true;
+		}
+		
+		return false;
+	}
+	
+
+	public enum AdminCommands {
+		
+		//TODO: CHECK THESE 
+		new_user_add, user_list, delete_user, extrauser_password_update, username_update,
+		disconnectotherconnections, showlog
+	}
+	
+
+	/**
+	 * @return true if given command is in the sub-set
+	 */
+	public static boolean requiresAdmin(PlayerCommands cmd){
+		for(AdminCommands admin : AdminCommands.values()){
+			if(admin.equals(cmd)) 
+				return true;
+		}
+		
+		return false;
+	}
+	
+	
 	@Override 
 	public String toString() {
 		return super.toString();

20d4520af6e7f93e581aa5981f74b9370db75b66
brad.zdanivsky <brad.zdanivsky@dc971243-0a89-dcf2-b47a-c166fffa500b>
2011-09-17 01:10:42 +0000

    bug fix, added IP to Records kept on logins

--- oculus/WEB-INF/src/oculus/LoginRecords.java
+++ oculus/WEB-INF/src/oculus/LoginRecords.java
@@ -13,46 +13,92 @@ public class LoginRecords {
 	public static State state = State.getReference();
 	public static final String PASSENGER = "passenger";
 	public static final String DRIVER = "driver";
-	private static final int MAX_RECORDS = 50;
+	public static final int MAX_RECORDS = 50;
+	
+	// TODO: only allow user to sign in once from a given ip??
+	// This should trigger an admin gmail warning if ppl share passwords?
 	
 	public LoginRecords() {}
 	
+	public void beDriver(String ip) {
+		
+		if(isConnected(state.get(State.user), ip))
+			System.out.println("user is allready connected!");
+		
+		list.add(new Record(state.get(State.user), DRIVER, ip));
+		state.set(State.userisconnected, true);
+		state.set(State.logintime, System.currentTimeMillis());
+	}
+	
+	public void bePassenger(String ip) {
+		list.add(new Record(state.get(State.user), PASSENGER, ip));
+		state.set(State.userisconnected, true);
+	}
+	
+
 	public void beDriver() {
-		list.add(new Record(state.get(State.user), DRIVER));
+		
+	//	if(isConnected(state.get(State.user), ip))
+		
+		list.add(new Record(state.get(State.user), DRIVER, "xxx.xxx.xxx.xxx"));
 		state.set(State.userisconnected, true);
 		state.set(State.logintime, System.currentTimeMillis());
 	}
 	
 	public void bePassenger() {
-		list.add(new Record(state.get(State.user), PASSENGER));
-		// state.set(State.userisconnected, true);
-		// state.set(State.logintime, System.currentTimeMillis());
+		list.add(new Record(state.get(State.user), PASSENGER, "xxx.xxx.xxx.xxx"));
+		state.set(State.userisconnected, true);
 	}
 
+	/** @return true if this user is allready connected from this address */ 
+	public boolean isConnected(final String user, final String ip){
+		for (int i = 0; i < list.size(); i++){
+			Record rec = list.get(i);
+			if (rec.isActive()){
+				if(rec.getAddress().equals(ip))
+					if(rec.getUser().equals(user))
+						return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public void signout() {
 		
-		//int sz = list.size();
+		if(state.getBoolean(State.developer)){
+			System.out.println("_logging out: " + state.get(State.user));
+			System.out.println("_waiting now:" + getPassengers());
+			System.out.println(toString());
+		}
 		
-		System.out.println("_logging out: " + state.get(State.user));
-		System.out.println("_size now:" + list.size());
-		System.out.println("_waiting now:" + getPassengers());
-		System.out.println(toString());
+		// try all instances
+		// int active = 0;
+		for (int i = 0; i < list.size(); i++){
+			Record rec = list.get(i);
+			if (rec.isActive()){
+				if (rec.getUser().equals(state.get(State.user))){
+					list.get(i).logout();
+				}
+			}
+		}
 		
-		// try all instances 
-		for (int i = 0; i < list.size(); i++)
-			if (list.get(i).getUser().equals(state.get(State.user)))
-				list.get(i).logout();
-		
-		System.out.println("size now:" + list.size());
-		System.out.println(toString());
-		
+		// assume this gets reset as new user logs in 
 		state.set(State.userisconnected, false);
 		state.delete(State.user);
 		
 		// maintain size limit 
 		if(list.size() > MAX_RECORDS) list.remove(0);
+		
+		if(state.getBoolean(State.developer)){
+			System.out.println("_logging out: " + state.get(State.user));
+			System.out.println("_waiting now:" + getPassengers());
+			System.out.println(toString());
+		}
+	
 	}
 
+	/** @return the number of users waiting in line */
 	public int getPassengers() {
 		int passengers = 0;
 		for (int i = 0; i < list.size(); i++){
@@ -63,11 +109,24 @@ public class LoginRecords {
 
 		return passengers;
 	}
+	
+	/** @return a list of user names waiting in line */
+	public String[] getPassengerList() {
+		String[] passengers = new String[getPassengers()];
+		for (int i = 0; i < list.size(); i++){
+			Record rec = list.get(i);
+			if(rec.isActive() && rec.isPassenger())
+				passengers[i] = rec.getUser();
+		}
+
+		return passengers;
+	}
 
 	public int size() {
 		return list.size();
 	}
 
+	/** create snap shot of current use to disk */
 	public static boolean save(){
 		
 		if(new File(filename).exists()) 
@@ -117,11 +176,15 @@ public class LoginRecords {
 		private long timeout = 0;
 		private String user = null;
 		private String role = null;
+		private String ip = null;
 		
-		Record(String usr, String role) {
+		Record(String usr, String role, String addr) {
 			this.user = usr;
 			this.role = role;
-			System.out.println("ceated login: " + toString());
+			this.ip = addr;
+			
+			if(state.getBoolean(State.developer))
+				System.out.println("ceated login: " + toString());
 		}
 
 		public String getUser() {
@@ -136,6 +199,10 @@ public class LoginRecords {
 			return timeout;
 		}
 		
+		public String getAddress(){
+			return ip;
+		}
+		
 		public boolean isActive(){
 			return (timeout==0);
 		}
@@ -146,7 +213,7 @@ public class LoginRecords {
 		
 		@Override
 		public String toString() {
-			String str = user + " " + role.toUpperCase() + " login: " + new Date(timein).toString();
+			String str = user + " " + role.toUpperCase() + " address: " + ip + " login: " + new Date(timein).toString();
 			if(isActive()) str += " is ACTIVE";
 			else str += " logout: " + new Date(timeout).toString();
 			
